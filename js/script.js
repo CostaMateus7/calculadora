cd34bf8cfad24b724adfdacac95467f61e479280
Mateus Costa <mateusscosta7@gmail.com>
2022-08-24 17:54:42 -0300

    configurando css e adicionando algumas funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-const botao = document.querySelectorAll('button')
-const resultado = document.querySelector('#resultados')
-botao.forEach((botao)=> {
-  botao.addEventListener('click', ()=>{
-    resultado.innerHTML += botao.innerHTML
+const botoes = document.querySelectorAll('.num')
+const resultados = document.querySelector('#resultados')
+let contas = document.querySelector('#contas')
+const reseta = document.querySelector('#reseta')
+const apagar = document.querySelector('#apaga')
+console.log(contas)
+
+
+apagar.addEventListener('click', ()=>{
+  let apagarNum = contas.innerHTML
+  contas.innerHTML = apagarNum.substring(0, contas.innerHTML.length - 1)
+})
+
+reseta.addEventListener('click', ()=>{
+  contas.innerHTML =''
+  resultados.innerHTML=''
+})
+
+
+
+botoes.forEach((botao)=> {
+  botao.addEventListener('click', (e)=>{
+    const valor = e.target.innerHTML
+    contas.innerHTML += valor
   })
 })
+
+// const button = document.querySelectorAll('button')
+// button.forEach((btn)=>{
+//   btn.addEventListener('click', (e)=>{
+//     const valor = e.target.innerText
+//     console.log(valor)
+//   })
+  
+// })
+
